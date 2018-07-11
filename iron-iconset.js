012c342f47072ba070a72e614832c444c91596df
Russell Bicknell <bicknellr@google.com>
2018-07-11 15:23:07 -0700

    Update `iron-iconset.js` comments.

--- iron-iconset.js
+++ iron-iconset.js
@@ -19,19 +19,18 @@ import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
  * [`iron-iconset-svg`](iron-iconset-svg).)
  *
  * The `src` property specifies the url of the icon image. Multiple icons may
- * be included in this image and they may be organized into rows.
- * The `icons` property is a space separated list of names corresponding to the
- * icons. The names must be ordered as the icons are ordered in the icon image.
- * Icons are expected to be square and are the size specified by the `size`
- * property. The `width` property corresponds to the width of the icon image
- * and must be specified if icons are arranged into multiple rows in the image.
+ * be included in this image and they may be organized into rows. The `icons`
+ * property is a space separated list of names corresponding to the icons. The
+ * names must be ordered as the icons are ordered in the icon image. Icons are
+ * expected to be square and are the size specified by the `size` property. The
+ * `width` property corresponds to the width of the icon image and must be
+ * specified if icons are arranged into multiple rows in the image.
  *
  * All `iron-iconset` elements are available for use by other `iron-iconset`
  * elements via a database keyed by id. Typically, an element author that wants
- * to support a set of custom icons uses a `iron-iconset` to retrieve
- * and use another, user-defined icon set, or simply uses the
- * [`iron-icon`](iron-icon) element to display an icon by specifying its icon
- * set name and id.
+ * to support a set of custom icons uses a `iron-iconset` to retrieve and use
+ * another, user-defined icon set, or simply uses the [`iron-icon`](iron-icon)
+ * element to display an icon by specifying its icon set name and id.
  *
  * Example:
  *
@@ -40,10 +39,10 @@ import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
  *     </iron-iconset>
  *
  * This will automatically register the icon set "my-icons" to the iconset
- * database.  To use these icons from within another element, make a
- * `iron-iconset` element and call the `byId` method to retrieve a
- * given iconset. To apply a particular icon to an element, use the
- * `applyIcon` method. For example:
+ * database. To use these icons from within another element, make a
+ * `iron-iconset` element and call the `byId` method to retrieve a given
+ * iconset. To apply a particular icon to an element, use the `applyIcon`
+ * method. For example:
  *
  *     iconset.applyIcon(iconNode, 'car');
  *
@@ -62,10 +61,9 @@ import {dom} from '@polymer/polymer/lib/legacy/polymer.dom.js';
  *
  * See also:
  *
- * -   [`iron-iconset-svg`](iron-iconset-svg). Build icon sets with SVG
- * elements.
- * -   [`iron-icons`](iron-icons). Predefined icon sets.
- * -   [`iron-icon`](iron-icon). Simple element to display an icon.
+ * - [`iron-iconset-svg`](iron-iconset-svg). Build icon sets with SVG elements.
+ * - [`iron-icons`](iron-icons). Predefined icon sets.
+ * - [`iron-icon`](iron-icon). Simple element to display an icon.
  *
  * @demo demo/index.html
  * @implements {Polymer.Iconset}
